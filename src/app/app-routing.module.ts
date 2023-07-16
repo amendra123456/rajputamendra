902846ce2c08b06242b8aa902594871c475e0da2
Amendra Rajput <amendra.rajput@velocis.co.in>
2023-07-16 12:26:04 +0530

    update changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,62 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SellerAuthComponent } from './seller-auth/seller-auth.component';
+import { SellerHomeComponent } from './seller-home/seller-home.component';
+import { authGuard } from './auth.guard';
+import { SellerAddProductComponent } from './seller-add-product/seller-add-product.component';
+import { SearchComponent } from './search/search.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { UserAuthComponent } from './user-auth/user-auth.component';
+import { CartPageComponent } from './cart-page/cart-page.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent   
+  },
+  {
+    path: 'seller-auth',
+    component: SellerAuthComponent   
+  },
+  {
+    path: 'seller-home',
+    component: SellerHomeComponent,
+    canActivate: [authGuard]
+  },
+  {
+    path: 'seller-add-product',
+    component: SellerAddProductComponent,
+    canActivate: [authGuard]   
+  },  
+  {
+    path: 'seller-add-product/:id',
+    component: SellerAddProductComponent,
+    canActivate: [authGuard]   
+  },
+  {
+    path: 'search/:query',
+    component: SearchComponent
+  },
+  {
+    path: 'user-auth',
+    component: UserAuthComponent
+  },
+  {
+    path: 'cart-page',
+    component: CartPageComponent
+  },
+  {
+    path: 'checkout',
+    component: CheckoutComponent
+  },
+  {
+    path: 'details/:productId',
+    component: ProductDetailsComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,48 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule, HttpClient, HTTP_INTERCEPTORS  } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CartPageComponent } from './cart-page/cart-page.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { HomeComponent } from './home/home.component';
+import { MyOrderComponent } from './my-order/my-order.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { SearchComponent } from './search/search.component';
+import { SellerAddProductComponent } from './seller-add-product/seller-add-product.component';
+import { SellerAuthComponent } from './seller-auth/seller-auth.component';
+import { SellerHomeComponent } from './seller-home/seller-home.component';
+import { UserAuthComponent } from './user-auth/user-auth.component';
+import { FormsModule } from '@angular/forms';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CartPageComponent,
+    CheckoutComponent,
+    HomeComponent,
+    MyOrderComponent,
+    ProductDetailsComponent,
+    SearchComponent,
+    SellerAddProductComponent,
+    SellerAuthComponent,
+    SellerHomeComponent,
+    UserAuthComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule,    
+    NgbModule,
+    
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
