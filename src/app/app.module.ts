046b50b3dfe9ea4c23430766441eccbd89d8a28a
Amendra Rajput <amendra.rajput@velocis.co.in>
2023-07-16 14:55:57 +0530

    update changes on project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { TermsconditionsComponent } from './termsconditions/termsconditions.component';
+import { FooterNewComponent } from './layout/footer-new/footer-new.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContactUsComponent,
+    AboutUsComponent,
+    TermsconditionsComponent,
+    FooterNewComponent
   ],
   imports: [
     BrowserModule,
